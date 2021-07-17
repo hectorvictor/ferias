270b27a4fc410e53b9f0c0203f1ea9a82878f75e
hectorvictor <victohector@gmail.com>
2021-07-17 13:25:31 -0300

    primeiro commit

---
+++ back/src/test/java/br/pratica/ferias/FeriasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pratica.ferias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
